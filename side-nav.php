24c6e70158e1ebf8d401531ae36faa505055abac
“[Daniel <[dmulih@gmail.com]>
2022-07-14 22:11:38 +0300

    New Additions

--- side-nav.php
+++ side-nav.php
@@ -21,19 +21,19 @@
             </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li <?php if(isset($page) && ($page=='products')) { echo 'class="active"'; } ?>><a href="products.php"><i class="fa fa-tasks"></i>View Products</a></li>
+                    <li <?php if(isset($page) && ($page=='staff')) { echo 'class="active"'; } ?>><a href="staff.php"><i class="fa fa-tasks"></i>View Staff</a></li>
                 </ul>
             </li>
-            <li class="treeview <?php if(isset($page) && ($page=='orders' || $page=='unprocessed_orders')) { echo 'active'; } ?>">
+            <li class="treeview <?php if(isset($page) && ($page=='roles' || $page=='create_roles')) { echo 'active'; } ?>">
                 <a href="#">
-                    <i class="fa fa-tasks"></i> <span>Order Management</span>
+                    <i class="fa fa-tasks"></i> <span>Roles Management</span>
                     <span class="pull-right-container">
               <i class="fa fa fa-angle-left pull-right"></i>
             </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li <?php if(isset($page) && ($page=='orders')) { echo 'class="active"'; } ?>><a href="orders.php"><i class="fa fa-tasks"></i>Order List</a></li>
-                    <li <?php if(isset($page) && ($page=='unprocessed_orders')) { echo 'class="active"'; } ?>><a href="unprocessed_orders.php"><i class="fa fa-tasks"></i>Unprocessed Re-orders</a></li>
+                    <li <?php if(isset($page) && ($page=='roles')) { echo 'class="active"'; } ?>><a href="roles.php"><i class="fa fa-tasks"></i>Roles</a></li>
+                    <li <?php if(isset($page) && ($page=='create_roles')) { echo 'class="active"'; } ?>><a href="create_roles.php"><i class="fa fa-tasks"></i>View Roles</a></li>
                 </ul>
             </li>
         </ul>
