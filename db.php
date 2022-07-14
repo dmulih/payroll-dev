a76c084986b72fb1fa9eb6a813e14fb95ecd404d
“[Daniel <[dmulih@gmail.com]>
2022-07-15 00:34:41 +0300

    Heroku db import

--- db.php
+++ db.php
@@ -1,32 +1,12 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-define('DBHOST', 'localhost'); // MySQL Database Host
-define('DBUSERNAME', 'payroll'); // MySQL Database Username
-define('DBPASSWORD', 'P@ssw0rd1'); // MySQL Database Password
-define('DBNAME', 'payroll-dev'); // MySQL Database Name
-/* Database Connection */
-$db = new mysqli(DBHOST,DBUSERNAME,DBPASSWORD,DBNAME);
-//echo '<pre>';var_dump($db);
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
-/* Database Connection Class - better for functions and all */
-class Database {
-    private static $db;
-    private $connection;
-    private function __construct() {
-        $this->connection = new MySQLi(DBHOST,DBUSERNAME,DBPASSWORD,DBNAME);
-    }
-    function __destruct() {
-        $this->connection->close();
-    }
-    public static function getConnection() {
-        if (self::$db == null) {
-            self::$db = new Database();
-        }
-        return self::$db->connection;
-    }
-}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
+$cleardb_username = $cleardb_url["be770652626c2e"];
+$cleardb_password = $cleardb_url["b3774d79"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
